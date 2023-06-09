0b55aad57e42c37ae4a522874205a9b0bf3af697
AmadouSY1 <105118435+AmadouSY1@users.noreply.github.com>
2023-06-09 14:41:03 +0000

    Delete rattrapage_design_pattern directory

--- rattrapage_design_pattern/src/main/java/com/example/rattrapage_design_pattern/repositories/EtudiantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rattrapage_design_pattern.repositories;
-
-import com.example.rattrapage_design_pattern.entity.Classe;
-import com.example.rattrapage_design_pattern.entity.Etudiant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EtudiantRepository extends JpaRepository<Etudiant,Long> {
-    Etudiant findByClasse(Classe classe);
-}
